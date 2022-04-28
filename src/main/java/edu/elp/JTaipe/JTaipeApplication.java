48185f6e6b6a407277fe1c4dbe96099b214b5de0
eltony46 <jackanthonytaipebarreto@gmail.com>
2022-04-27 19:47:03 -1200

    examen

---
+++ src/main/java/edu/elp/JTaipe/JTaipeApplication.java
@@ -0,0 +1,13 @@
+package edu.elp.JTaipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTaipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JTaipeApplication.class, args);
+	}
+
+}
